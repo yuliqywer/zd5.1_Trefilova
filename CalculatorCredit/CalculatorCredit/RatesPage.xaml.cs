088a2985b782bd2e5ca2248ad06e20752080ca94
yuliqywer <vikatrefilova8484@gmail.com>
2026-06-11 18:34:13 +0500

    Первый коммит

---
+++ CalculatorCredit/CalculatorCredit/RatesPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorCredit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RatesPage : ContentPage
+    {
+        public RatesPage()
+        {
+            InitializeComponent();
+            CurrencyDatePicker.Date = new DateTime(2026, 6, 11);
+        }
+    }
+}
